f3f02f3230c961bb6a9f9a36f3e3215c83834ec8
James Reynolds <jameshreynolds@gmail.com>
2017-08-28 17:59:54 -0700

    Added question counter to prompts.

--- JS/main.js
+++ JS/main.js
@@ -6,9 +6,16 @@ var adjective;
 
 // Example story: There once was a 'handsom(adj)' 'dog(noun)' who would 'worked(verb)' really hard everyday. 
 
-noun = prompt("Enter a noun");
-verb = prompt("Enter a verb");
-adjective = prompt("Enter an adjective")
+var questions = 3;
+var questionsLeft = ' [' + questions + ' questions left]'
+questions -= 1
+noun = prompt("Enter a noun" + questionsLeft);
+var questionsLeft = ' [' + questions + ' questions left]'
+questions -= 1
+verb = prompt("Enter a verb" + questionsLeft);
+var questionsLeft = ' [' + questions + ' questions left]'
+questions -= 1
+adjective = prompt("Enter an adjective" + questionsLeft);
 
 var story = "There once was a " + adjective + " " + noun + " who would " + verb + " really hard everyday.";
 
